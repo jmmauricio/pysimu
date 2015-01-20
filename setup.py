807f060106b65cdc2345fab9e70bd11352a447d5
jmmauricio <jmmauricio6@gmail.com>
2015-01-20 15:15:13 +0100

    new commit

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version = "0.0.2c"
+version = "0.0.2d"
 
 setup(
     name = "pysimu",
